8a324e44a16d01f206a7a536eeda9cd038a0c50d
Mikhail Slabchenko <m.s.slabchenko@diacare-soft.ru>
2020-04-01 15:33:06 +0300

    test binary search usage

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/errorboi/go-grokking-algorithms
+
+go 1.14
+
+require github.com/ErrorBoi/go-grokking-algorithms v0.0.0-20200401122257-4076fa2b4b34
--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	bs "github.com/ErrorBoi/go-grokking-algorithms/01_BinarySearch"
+)
+
+func main() {
+	testBinarySearch()
+}
+
+func testBinarySearch() {
+	arr := []int{1, 2, 5, 6, 9}
+	item := 6
+	res := bs.BinarySearch(arr, item)
+	fmt.Printf("Item %d is located at index %d\n", item, res)
 }
