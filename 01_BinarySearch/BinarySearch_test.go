5747d6d58bb6a94f4c203c0c98c142df6044d70f
Mikhail Slabchenko <m.s.slabchenko@diacare-soft.ru>
2020-04-01 17:14:57 +0300

    removed main.go, expanded readme

--- 01_BinarySearch/BinarySearch_test.go
+++ 01_BinarySearch/BinarySearch_test.go
@@ -4,10 +4,23 @@ import "testing"
 
 func TestBinarySearch(t *testing.T) {
 	arr := []int{1, 2, 5, 6, 9}
+
 	t.Run("Item was found by index 3", func(t *testing.T) {
 		item := 6
 		if res := BinarySearch(arr, item); res != 3 {
 			t.Errorf("Item was found by index %d, want 3", res)
+		}
+	})
+	t.Run("Item was found by index 0", func(t *testing.T) {
+		item := 1
+		if res := BinarySearch(arr, item); res != 0 {
+			t.Errorf("Item was found by index %d, want 0", res)
+		}
+	})
+	t.Run("Item was not found", func(t *testing.T) {
+		item := 3
+		if res := BinarySearch(arr, item); res != -1 {
+			t.Errorf("Item was found by index %d, want -1 (Not Found)", res)
 		}
 	})
 }
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # go-grokking-algorithms
 Go (Golang) code samples for Grokking Algorithm book by Aditya Y. Bhargava
+## Proof of work
+You can make sure it works by running tests by running tests, which are located in directory with every code sample.
+
+Run them all at once:
+
+`go test -count=1 -v ./...`
+
+Run specific test suite:
+
+`go test -count=1 -v location (e.g. ./01_BinarySearch)`
